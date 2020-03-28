f23568c083c6507ae260b7b72042c7e3dad67fbd
Niels van der Linden <linden@localhost.localdomain>
2020-03-28 19:19:23 +0100

    Feature: working version, contain runnable bash script, readme, license, and samplefile

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# clockout
+# Clockout
 An aid in text-based time logging
+
+## Installation
+Make sure clockout.bash has executable permission. If this not possible, you can run it by calling bash first.
+
+Dependencies:
+- bash
+- egrep
+- bc
+- units
+
+## Usage
+A text-based inputfile is expected according to the following format:
+```
+ABC-1           09:00   09:10
+DEF-1           09:10   09:20
+ABC-1           09:35   10:55
+```
+Any type and amount of whitespace characters are allowed, as long as there are three columns on each line. Empty lines are not allowed.
+
+Running the program from CLI:
+`./clockout.bash <inputfile>`
+
+The output of the above sample would look like:
+```
+ABC-1				09:00	09:10	10
+ABC-1				09:35	10:55	80
+└ Subtotal: 90
+
+DEF-1				09:10	09:20	10
+
+Total: 	1 hr + 40 min
+```
+
+The calculated timesheet is outputted to Stdout, if dependencies are met and the inputfile conforms to the expected format. The output can be piped to another file, if desired.
+
+## Possible improvements
+
+- Add tests
+- Support comments
+- Add a test for timelog overlap
+
+## Contributing
+
+Feel free to open an issue or a pull request.
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ clockout.bash
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Test dependencies
+if ! [ -x "$(command -v bc)" ]; then
+    echo 'Error: bc is not installed.' >&2
+    exit 1
+fi
+if ! [ -x "$(command -v units)" ]; then
+    echo 'Error: units not installed.' >&2
+    exit 1
+fi
+
+# Test input file
+if [ "$1" = "" ]; then
+    echo "Error: no input file provided" >&2
+    exit 1
+fi
+#DATA=$(cat $1)
+#cat $1 | while read line
+while read -r line
+do
+    if ! echo "$line" | egrep -q '^[^s]+\s+[0-2][0-9]:[0-5][0-9]\s+[0-2][0-9]:[0-5][0-9]$'; then
+        echo -e "Error: input file has incorrect format on line:\n$line" >&2
+        exit 1
+    fi
+done < $1
+
+# Clockout
+TOTAL=0
+PREVIOUS_STORY=""
+PREVIOUS_DURATION=0
+SUBTOTAL=0
+DATA=$(cat $1 | sort)
+DATA+="\n"
+echo -e "$DATA" | { while read line
+do
+    STORY=$(echo $line | awk '{print $1}')
+    FROM=$(echo $line | awk '{print $2}')
+    TO=$(echo $line | awk '{print $3}')
+    FROM_EPOCH=$(date +%s --date=$FROM)
+    TO_EPOCH=$(date +%s --date=$TO)
+    DURATION=$(echo "($TO_EPOCH - $FROM_EPOCH) / 60.0" | bc)
+    TOTAL=$(echo "$TOTAL + $DURATION" | bc)
+    if [ "$STORY" = "$PREVIOUS_STORY" ]; then
+        SUBTOTAL=$(echo "$SUBTOTAL + $DURATION" | bc)
+    else
+        if [ $SUBTOTAL -gt $PREVIOUS_DURATION ]; then
+            echo -e "└ Subtotal: $SUBTOTAL"
+        fi
+        if [ "$PREVIOUS_STORY" != "" ]; then
+            echo
+        fi
+        SUBTOTAL=$DURATION
+    fi
+    if [ "$line" != "" ]; then
+        echo -e "$STORY\t\t\t\t$FROM\t$TO\t$DURATION"
+    fi
+    PREVIOUS_STORY=$STORY
+    PREVIOUS_DURATION=$DURATION
+done
+echo -e "Total: $(units "$TOTAL minutes" time)"
+}
